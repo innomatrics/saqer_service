6d9b8504e8c459619a679ab73ebb347d7092f064
Sathish g <srisrisrisathish176@gmail.com>
2025-07-14 14:42:16 +0530

    sundalman

--- lib/screens/driver/screens/driver wallet/driver_wallet_screen.dart
+++ lib/screens/driver/screens/driver wallet/driver_wallet_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:saqer_services/constants/constants.dart';
 
 class DriverWalletScreen extends StatefulWidget {
   const DriverWalletScreen({super.key});
@@ -10,6 +11,79 @@ class DriverWalletScreen extends StatefulWidget {
 class _DriverWalletScreenState extends State<DriverWalletScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: size.height * 0.02,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 15),
+              const Text(
+                "Wallet",
+                style: TextStyle(
+                  color: AppColors.mainColor,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(16),
+                height: size.height * 0.3,
+                width: size.width * 1,
+                decoration: BoxDecoration(
+                  color: const Color(0xffF4F6FA),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      blurRadius: 10,
+                      offset: const Offset(0, 6),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Balance",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Container(
+                          height: size.height * 0.04,
+                          width: size.width * 0.1,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: AppColors.mainColor,
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "\$",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
